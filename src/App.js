2e52b23d299669d3c45f036505c7c0b0da388ced
sbkim1126 <subink1126@gmail.com>
2023-07-26 17:53:26 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useMemo } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const isLimit = useMemo(() => {
+    return todoList.length >= 8;
+  }, [todoList]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    if (!isLimit) {
+      setTodoList((current) => {
+        return [...current, {
+          value: inputValue,
+          isCompleted: false,
+        }]
+      });
+      setInputValue('');
+    }
+  }
+
+  const handleCompletedClick = (index) => {
+    setTodoList((current) => {
+      const newList = [...current];
+      newList[index] = { ...newList[index], isCompleted: !newList[index].isCompleted };
+      return newList;
+    })
+  }
+
+  const handleRemoveClick = (index) => {
+    setTodoList((current) => {
+      const newList = [...current];
+      newList.splice(index, 1);
+      return newList;
+    })
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <h2>디자인 몰?루</h2>
+        <input 
+        type='text' 
+        value={inputValue} 
+        onChange={e => setInputValue(e.target.value)}
+        disabled={isLimit} />
+        <button type='submit'>등록</button>
+      </form>
+
+      <ol>
+        {todoList.map((item, index) => (
+          <li className={item.isCompleted === true ? 'completed' : ''}>
+            <span>{item.value}</span>
+            <button onClick={() => { handleCompletedClick(index) }}>완료</button>
+            <button onClick={() => { handleRemoveClick(index) }}>삭제</button>
+          </li>
+        ))}
+      </ol>
+
+      {isLimit && (
+        <>
+        <div>할 일 너무 많음</div>
+        </>
+      )}
     </div>
   );
 }
